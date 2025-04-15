81f3e1ae09d56070fb322371f304e8f9869a28f1
Ali Ashooriyoon <aliashooriyoon@yahoo.com>
2025-04-15 21:36:56 +0330

    show items true in cart

--- src/components/pages/Cart/Cart.jsx
+++ src/components/pages/Cart/Cart.jsx
@@ -3,20 +3,41 @@ import { useDataCart } from "../../../useDataCart.jsx";
 
 const Cart = () => {
   const { products } = useDataCart();
-  const [cart, setCart] = useState(products);
+  const [cart, setCart] = useState([]);
   const [showCart, setShowCart] = useState(false);
   useEffect(() => {
+    const newMap = new Map();
     console.log(products);
+    products.forEach((element) => {
+      newMap.set(`item${element.id}`, element);
+    });
+    console.log([...newMap]);
+    console.log(Array.from(newMap));
+    const arrForm = [...newMap];
+    const secondItem = arrForm.map((i) => i[1]);
+    setCart(secondItem);
+    console.log(secondItem);
+    console.log(newMap);
+  }, [products]);
+  useEffect(() => {
     console.log(cart);
-    setCart(products);
-  });
+  }, [cart]);
+  // --------------------------------------
   return (
     <>
       <div onClick={() => setShowCart(!showCart)} className="cart">
-        Cart ( {products ? products.length : 0} )
+        Cart ( {cart ? cart.length : 0} )
       </div>
-      {showCart &&
-        cart.map((item) => <div className="cartBoxShower">{item.name}</div>)}
+      {showCart && (
+        <div className="itemsBox absolute top-16">
+          {cart &&
+            cart.map((item) => (
+              <div className="cartBoxShower absolu" key={item.id}>
+                {item.name}
+              </div>
+            ))}
+        </div>
+      )}
     </>
   );
 };
