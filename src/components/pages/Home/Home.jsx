43a6ea309635730f11de0d7e9c701b261ae64bf8
Ali Ashooriyoon <aliashooriyoon@yahoo.com>
2025-05-12 22:26:35 +0330

    start using tailwind

--- src/components/pages/Home/Home.jsx
+++ src/components/pages/Home/Home.jsx
@@ -3,7 +3,7 @@ import { BoxProduct } from "../BoxProduct/BoxProduct.jsx";
 const Home = () => {
   return (
     <>
-      <div className="home">
+      <div className="home 2xl:bg-red-700">
         <BoxProduct />
       </div>
     </>
